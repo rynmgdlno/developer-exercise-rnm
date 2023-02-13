dc5f5f48e09c23dce2a6144e58340392bbbfa80c
rynmgdlno <ryannmagdaleno@icloud.com>
2023-02-13 09:59:19 -0800

    Implement: Even Fibonacci Sum function

--- exercise.rb
+++ exercise.rb
@@ -22,11 +22,32 @@ class Exercise
     return result
   end
 
+  # Fibonacci helper. Generates complete fib sequence to n values
+  private_class_method def self.get_fibonacci(nth)
+      a, b, sum = 0, 1, 0
+      result = Array.new(1, 1)
+      for x in 1...nth
+        sum = a + b
+        a = b
+        b = sum
+        result.push(sum)
+      end
+      return result
+    end
+
   # Return the sum of all even numbers in the Fibonacci sequence, up to
   # the "nth" term in the sequence
   # eg. the Fibonacci sequence up to 6 terms is (1, 1, 2, 3, 5, 8),
   # and the sum of its even numbers is (2 + 8) = 10
   def self.even_fibonacci(nth)
-    # TODO: Implement this method
+    # generate complete fib sequence for n values
+    fib = get_fibonacci(nth)
+
+    # filtering out odd values
+    fibEvenOnly = fib.filter { |val| val % 2 == 0 }
+
+    # find and return sum of remaining (even) values
+    result = fibEvenOnly.reduce(0) { |sum, val| sum + val }
+    return result
   end
 end
