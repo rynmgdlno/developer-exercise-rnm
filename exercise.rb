fcfe074752bb9272767c428b258dca79a9cb7ce9
rynmgdlno <ryannmagdaleno@icloud.com>
2023-02-13 09:58:28 -0800

    Implement: Marklar function

--- exercise.rb
+++ exercise.rb
@@ -4,7 +4,22 @@ class Exercise
   # Return a string in which every word in "str" that exceeds 4 characters is replaced with "marklar".
   # If the word being replaced has a capital first letter, it should instead be replaced with "Marklar".
   def self.marklar(str)
-    # TODO: Implement this method
+    punctuation = ["!", ".", ",", "?", ":", ";", "-"]
+    marklared = str.split(" ").map do |word|
+      if word.length > 4
+        # if word ends with a punctuation mark, store it in punc:
+        punc = punctuation.include?(word[-1]) ? word[-1] : ""
+        if !!word[0].match(/\p{Upper}/)
+          "Marklar#{punc}"
+        else
+          "marklar#{punc}"
+        end
+      else
+        word
+      end
+    end
+    result = marklared.join(" ")
+    return result
   end
 
   # Return the sum of all even numbers in the Fibonacci sequence, up to
@@ -14,5 +29,4 @@ class Exercise
   def self.even_fibonacci(nth)
     # TODO: Implement this method
   end
-
 end
